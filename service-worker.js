7101f1d99a4d5d5a839ca5274f949e4a79c9eb69
pankajyoutbers9528-alt <pankajyoutbers9528@gmail.com>
2026-09-26 07:45:46 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "suraksha-alert-v3";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
